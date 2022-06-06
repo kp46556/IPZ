95957b9e23b86ab316a6ad99daf482420d4566c4
Piotr <kp46556@zut.edu.pl>
2022-06-06 03:41:19 +0200

    Działająca wersja dev z autentykacją "Piekło też jest dla człowieka" - Epitafium dla Wysockiego

--- gateway-service/src/main/java/ipz/gatewayservice/repositories/IOfficeRepository.java
+++
@@ -1,9 +0,0 @@
-package ipz.gatewayservice.repositories;
-
-import ipz.gatewayservice.models.Office;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IOfficeRepository extends JpaRepository<Office, Long> {
-}
--- health-service/src/main/java/ipz/healthservice/repositories/IDoctorRepository.java
+++ health-service/src/main/java/ipz/healthservice/repositories/IDoctorRepository.java
@@ -2,10 +2,11 @@ package ipz.healthservice.repositories;
 
 import ipz.healthservice.models.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
+@Repository
 public interface IDoctorRepository extends JpaRepository<Doctor, Long> {
     List<Doctor> findAllByBranchId(Long id);
 }
